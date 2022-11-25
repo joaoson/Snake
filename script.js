353586e5d6adb0f04263cfd90f1135f9ef55a04a
Joaofre <joaovitor6586@gmail.com>
2022-11-25 16:25:38 -0300

    added fruits, h1 element, button and extra features

--- script.js
+++ script.js
@@ -3,11 +3,16 @@ let context = canvas.getContext("2d");
 let square = 32;
 let snake= [];
 
-let direction= "right";
+let direction= "";
 
 snake[0]={
     x: 8 * square,
     y: 8 * square
+}
+
+let food= {
+    x: Math.floor(Math.random() *15 + 1) * square,
+    y: Math.floor(Math.random() *15 + 1) * square
 }
 
 document.addEventListener('keydown',update);
@@ -20,7 +25,7 @@ function update(event){
 }
 
 function createBg(){
-    context.fillStyle= 'blue';
+    context.fillStyle= 'lightgreen';
     context.fillRect(0,0,16 * square, 16 * square)
 }
 
@@ -31,14 +36,26 @@ function createSnake(){
     }
 }
 
+function createFood(){
+    context.fillStyle= 'red';
+    context.fillRect(food.x,food.y,square,square)
+}
+
 function startGame(){
     if(snake[0].x > 15 * square && direction == 'right') snake[0].x = 0;
     if(snake[0].x < 0  && direction == 'left') snake[0].x = 16 * square;
     if(snake[0].y > 15 * square && direction == 'down') snake[0].y = 0;
     if(snake[0].y < 0 && direction == 'up') snake[0].y = 16 * square;
 
+    for(i=1;i < snake.length;i++){
+        if(snake[0].x == snake[i].x && snake[0].y == snake[i].y){
+            clearInterval(jogo)
+        }
+    }
+
     createBg();
     createSnake();
+    createFood();
 
     let snakeX = snake[0].x;
     let snakeY = snake[0].y;
@@ -48,7 +65,14 @@ function startGame(){
     if(direction == "up") snakeY -= square;
     if(direction == "down") snakeY += square;
 
-    snake.pop()
+    if(snakeX != food.x || snakeY != food.y){
+        snake.pop()
+    } else {
+        food.x = Math.floor(Math.random() *15 + 1) * square;
+        food.y = Math.floor(Math.random() *15 + 1) * square;
+    }
+
+    
 
     let newhead = {
         x: snakeX,
@@ -59,5 +83,13 @@ function startGame(){
 
 }
 
-let jogo = setInterval(startGame,50);
+let iniciar = document.getElementById('start')
+iniciar.addEventListener('click',iniciarJogo)
+
+function iniciarJogo(event){
+    let jogo = setInterval(startGame,100);
+    iniciar.classList.add('hidden')
+}
+
+
 
